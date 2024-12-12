a9c086236c72101f5d45ecaad938d73e796a9cc7
soofka <pansoofka@gmail.com>
2024-12-12 19:10:44 +0100

    attempt to read file in netlify function

--- netlify/functions/hello.mjs
+++ netlify/functions/hello.mjs
@@ -1,3 +1,25 @@
+import fs from "fs/promises";
+import path from "path";
+
 export default async (req, context) => {
-  return new Response("Hello, world!");
+  let signups = [];
+  const filePath = path.resolve("./signups.json");
+  try {
+    signups = JSON.parse(await fs.readFile(filePath, { encoding: "utf8" }));
+    signups.push(Date.now());
+    await fs.writeFile(filePath, JSON.stringify(signups), { encoding: "utf8" });
+    return new Response(signups);
+  } catch (readingError) {
+    if (error.code && error.code === "ENOENT") {
+      try {
+        await fs.writeFile(filePath, JSON.stringify(signups), {
+          encoding: "utf8",
+        });
+        return new Response(signups);
+      } catch (writingError) {
+        return new Response(writingError.message, { status: 500 });
+      }
+    }
+    return new Response(readingError.message, { status: 500 });
+  }
 };

363e3d9e9b628d681c7a76e13d4278c9f6b4ac39
FundacjaSafeSpot <safespot.fundacja@gmail.com>
2025-09-09 19:50:39 +0200

    Update orange-banner.js

--- src/static/js/orange-banner.js
+++ src/static/js/orange-banner.js
@@ -2,11 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const banner = document.getElementById('orange-banner');
   if (banner) {
     const closeBtn = banner.querySelector('.close-banner');
-    closeBtn.addEventListener('click', () => {
-      banner.classList.add('hidden');
-      setTimeout(() => {
-        banner.style.display = 'none';
-      }, 600);
-    });
+    if (closeBtn) {
+      closeBtn.addEventListener('click', () => {
+        banner.classList.add('hidden');
+        setTimeout(() => {
+          banner.style.display = 'none';
+        }, 600); 
+      });
+    }
   }
 });
+
